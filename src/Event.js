0b2a3cfbead85d02c06e6ff536aa28afb643ef6d
Matt Dillemuth <mattdillemuth@gmail.com>
2021-01-06 22:22:04 -0500

    Add test cases for expand/collapse event on click

--- src/Event.js
+++ src/Event.js
@@ -1,8 +1,50 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    isExpanded: false,
+  };
+
+  // Toggle expand/collapse event on click
+  handleExpandEvent = () => {
+    if (this.state.isExpanded === false) {
+      this.setState({
+        isExpanded: true,
+      });
+    } else {
+      this.setState({
+        isExpanded: false,
+      });
+    }
+  };
+
+  renderExpandedComponent = () => {
+    if (this.state.isExpanded) {
+      return (
+        <div className='Expanded-Event'>
+          <div className='about'></div>
+          <div className='link'></div>
+          <div className='description'></div>
+        </div>
+      );
+    }
+  };
+
   render() {
-    return <div></div>;
+    return (
+      <div className='Event'>
+        <div className='summary'></div>
+        <div className='start-dateTime'></div>
+        <div className='start-timeZone'></div>
+        <div className='location'></div>
+        <button
+          type='button'
+          className='btn-details'
+          onClick={this.handleExpandEvent}
+        ></button>
+        {this.renderExpandedComponent()}
+      </div>
+    );
   }
 }
 
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -27,4 +27,38 @@ describe('<Event /> component', () => {
   test('should render details button', () => {
     expect(EventWrapper.find('.btn-details')).toHaveLength(1);
   });
+
+  test('should render event component correctly', () => {
+    expect(EventWrapper.state('isExpanded')).toBe(false);
+  });
+
+  test('clicking "show-details" button should expand event component', () => {
+    EventWrapper.find('.btn-details').simulate('click');
+    expect(EventWrapper.state('isExpanded')).toBe(true);
+  });
+
+  test('should render expanded event component correctly', () => {
+    expect(EventWrapper.find('.Expanded-Event')).toHaveLength(1);
+  });
+
+  test('expanded event should render "About Event" header', () => {
+    expect(EventWrapper.find('.about')).toHaveLength(1);
+  });
+
+  test('expanded event should render htmlLink ("see details on Google Calendar")', () => {
+    expect(EventWrapper.find('.link')).toHaveLength(1);
+  });
+
+  test('expanded event should render event description', () => {
+    expect(EventWrapper.find('.description')).toHaveLength(1);
+  });
+
+  test('expanded event should collapse when details button is clicked', () => {
+    EventWrapper.find('.btn-details').simulate('click');
+    expect(EventWrapper.state('isExpanded')).toBe(false);
+  });
+
+  test('expanded event component should no longer be present after collapsing event', () => {
+    expect(EventWrapper.find('.Expanded-Event')).toHaveLength(0);
+  });
 });
