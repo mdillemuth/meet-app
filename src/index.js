9c1ba5d71cc0c212da31af1a9aa1ebca5d005240
Matt Dillemuth <mattdillemuth@gmail.com>
2021-01-19 15:28:30 -0500

    Add Atatus application performance monitoring (APM)

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
-import React from "react";
-import ReactDOM from "react-dom";
-import App from "./App";
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+import * as atatus from 'atatus-spa';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  document.getElementById('root')
 );
 
+atatus.config('b206faf0f0394dfcac9e7e53568bc5e8').install();
 // reportWebVitals();
