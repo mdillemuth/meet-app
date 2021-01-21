afa94855728b239ec094ae9bc2cbc5df85a13169
Matt Dillemuth <mattdillemuth@gmail.com>
2021-01-21 11:19:14 -0500

    Update manifest and add serviceWorker

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as atatus from 'atatus-spa';
+import * as serviceWorker from './serviceWorker';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -13,3 +14,5 @@ ReactDOM.render(
 
 atatus.config('b206faf0f0394dfcac9e7e53568bc5e8').install();
 // reportWebVitals();
+
+serviceWorker.register();
