815f8ae6f6460645cb7068859e2e3a731affff79
Matt Dillemuth <mattdillemuth@gmail.com>
2021-01-21 12:30:30 -0500

    Update service-worker and add workbox packages

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 import * as atatus from 'atatus-spa';
-import * as serviceWorker from './serviceWorker';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 // import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
@@ -13,6 +13,13 @@ ReactDOM.render(
 );
 
 atatus.config('b206faf0f0394dfcac9e7e53568bc5e8').install();
-// reportWebVitals();
 
-serviceWorker.register();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://cra.link/PWA
+serviceWorkerRegistration.register();
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+// reportWebVitals();
